d04cb720cd6e08139da13e239f86410fa5cc8278
Kelly08-Z <kz.osorto@gmail.com>
2020-09-12 02:52:28 -0500

    proyecto terminado 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { AlertsComponent } from './alerts/alerts.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AlertsComponent ],
+  declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
